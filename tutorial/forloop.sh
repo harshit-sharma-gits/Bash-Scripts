4b56c196d9c4c2201cceda84dc4ff897d55904b4
Harshit Sharma <harshits908@gmail.com>
2023-02-13 22:01:01 +0530

    More lessons taken

---
+++ tutorial/forloop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for curr_no in {1..10}
+do
+	echo $curr_no
+	sleep 0.7
+done
+
+echo "We are outside the for loop"
---
+++ tutorial/whileLoop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+myvar=1
+
+while [ $myvar  -le 10 ]
+do
+	echo $myvar
+	myvar=$(( $myvar + 1 ))
+	sleep 0.5
+done

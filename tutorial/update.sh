70b24ec0dc8547536c28f97f8d0931d7bea4c804
Harshit Sharma <harshits908@gmail.com>
2023-03-18 22:12:48 +0530

    Added function in update.sh

--- tutorial/update.sh
+++ tutorial/update.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=./logfile.log
+errorlog=./errorlog.log
+
+check_exit_status() {
+	if [ $? -ne 0 ]
+	then
+		echo "An error occured, please check $errorlog file"
+	fi
+}
 
 if grep -q "Arch" $release_file
 then
 	# host is based on Arch
-	sudo pacman -Syu
+	sudo pacman -Syu 1>>$logfile 2>>$errorlog
+	check_exit_status
 fi
 
 
@@ -13,5 +23,8 @@ if grep -q "Ubuntu" $release_file || grep -q "Debian" $release_file
 then
 	# host is based on Debian / Ubuntu
 	sudo apt update
+	check_exit_status
+
 	sudo apt dist-upgrade
+	check_exit_status
 fi
